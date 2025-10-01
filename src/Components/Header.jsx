9fca08ca187270fa4742da6eea56267849a5908c
Chathurangi <you@example.com>
2025-10-01 23:16:38 +0530

    refactor: update Header component layout and styling for improved navigation

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,17 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
-export default function Header (){
+export default function Header() {
   return (
-    <div className='flex items-center justify-between h-20 max-w-6xl mx-auto'>
-        <Link to={'/'}>
-        <div className='ml-5'>
-            <h1 className='text-red-900 font-bold text-xl sm:text-2xl md:text-3xl cursor-pointer tracking-wide'>React Redux Shopping cart</h1>
-        </div>
+    <nav className="flex items-center justify-evenly h-20 px-8 bg-white shadow-md">
+      <Link to="/" className="flex items-center">
+        <h1 className="text-red-900 font-bold text-2xl tracking-wide hover:text-gray-700 transition-colors duration-200">
+          Redux Shopping Cart
+        </h1>
+      </Link>
+      <div className="flex items-center w-[800px] justify-evenly space-x-4 font-semibold">
+        <Link
+          to="/"
+          className=" text-gray-700 hover:text-gray-700 transition-colors duration-200 space-x-5"
+        >
+          Home
         </Link>
-
-
-
-    </div>
-  )
+        <Link
+          to="/cart"
+          className="text-gray-700 hover:text-gray-700 transition-colors duration-200"
+        >
+          Cart
+        </Link>
+      </div>
+    </nav>
+  );
 }
+
